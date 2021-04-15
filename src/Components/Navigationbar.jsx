1157e9162a972b32b41c2c263a8cadfe7cd69a9c
kevin-lane <kevinlane3@hotmail.com>
2021-04-16 01:11:15 +0200

    LinkedIn SVG Icon linking to my LinkedIn profile in navbar

--- src/Components/Navigationbar.jsx
+++ src/Components/Navigationbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import  LinkedIn  from '../assets/svg/LinkedInIcon';
 
 export default function NavigationBar(){
     console.log(window.location.pathname);
@@ -9,7 +10,9 @@ export default function NavigationBar(){
                 <a href="/">Home</a>
                 <a href="about">About</a>
                 <a href="contact">Contact</a>
+                <a className="svgNavs" href="https://www.linkedin.com/in/kevin-lane-884950a7/" target="_blank"><LinkedIn></LinkedIn></a>
             </ul>
+            
         </div>
     )
 }
