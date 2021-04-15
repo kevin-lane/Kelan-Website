f5f91f2fa4d6df28988b8985a0c0e709f5829ee8
kevin-lane <kevinlane3@hotmail.com>
2021-04-15 22:24:18 +0200

    Added pointer as a cursor to Send Message button

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
             <h1>Welcome to my page</h1>
             <p>Kevin is my name and I am currently a junior web developer with knowledge in SharePoint based in Stockholm, Sweden</p>
             <p>This website will act like my CV including my portfolio</p>
-            <p>(PAGE IS CURRENTLY UNDER CONSTRUCTION)</p>
+            <p>(PAGE IS CURRENTLY UNDER CONSTRUCTION SO NOT EVERYTHING MIGHT WORK)</p>
         </div>
     );
 }
