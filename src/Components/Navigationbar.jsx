10d6389214187d6567d8daa2ae84fc4965669a77
kevin-lane <kevinlane3@hotmail.com>
2021-04-25 16:36:45 +0200

    Use capital letters in main navigation bar for a clearer user experience

--- src/Components/Navigationbar.jsx
+++ src/Components/Navigationbar.jsx
@@ -7,12 +7,11 @@ export default function NavigationBar(){
     return(
         <div className="Navbar">
             <ul>
-                <a href="/">Home</a>
-                <a href="about">About</a>
-                <a href="contact">Contact</a>
+                <a href="/">HOME</a>
+                <a href="about">ABOUT</a>
+                <a href="contact">CONTACT</a>
                 <a className="svgNavs" href="https://www.linkedin.com/in/kevin-lane-884950a7/" target="_blank" rel="noreferrer"><LinkedIn></LinkedIn></a>
             </ul>
-            
         </div>
     )
 }
