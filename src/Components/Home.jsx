e3aafb000b3ce8cfa9912e78c12e02104a5cb2fe
kevin-lane <kevinlane3@hotmail.com>
2021-04-25 17:31:50 +0200

    Changed the content on main page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,11 @@ import '../App.css';
 export default function Home(){
     return(
         <div>
-            <h1>Welcome to my page</h1>
-            <p>Kevin is my name and I am currently a junior web developer with knowledge in SharePoint based in Stockholm, Sweden</p>
+            <h1>KEVIN LANE</h1>
+            <hr></hr>
+            <p>Junior JavaScript Developer with focus on Office365 and a passion for React</p>
             <p>This website will act like my CV including my portfolio</p>
-            <p>(PAGE IS CURRENTLY UNDER CONSTRUCTION SO NOT EVERYTHING MIGHT WORK)</p>
+            <p>(PAGE IS CURRENTLY UNDER CONSTRUCTION SO EVERYTHING MIGHT NOT FUNCTION PROPERLY)</p>
         </div>
     );
 }
