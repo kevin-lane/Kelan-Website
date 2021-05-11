d08866704dd7e245a8ceefc0f175403f524d0aa0
kevin-lane <kevinlane3@hotmail.com>
2021-05-12 00:54:21 +0200

    Blog and Portfolio site added to the project with ability to navigate to

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import NavigationBar from './Components/Navigationbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+import Blog from './Components/Blog';
 
 function App() {
   function switchPage(){
     switch (window.location.pathname) {
       case '/about':
         return( <About />);
+      case '/blog':
+        return( <Blog /> )
       case '/contact':
         return( <Contact />);
+      case '/portfolio':
+        return( <Portfolio /> )
       default:
         return(<Home />);
     }
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+export default function Portfolio(){
+    return(
+        <div>
+            <h1>Posts will be coming soon or site is under construction</h1>
+        </div>
+    );
+}
