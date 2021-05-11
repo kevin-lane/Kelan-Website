35295dec3ddbea7439f368422bc8805c52455ab3
kevin-lane <kevinlane3@hotmail.com>
2021-05-12 00:19:14 +0200

    First release of main page with some awesome CSS animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,17 +12,20 @@ export default function Home(){
     });
 
     return(
-        <div id="home-under-layer">
-            <div>
-                <div>
-                    <h1 >KEVIN LANE</h1>
-                    <h3>JavaScript and Office365 Developer</h3>
-                    <p>Junior JavaScript Developer with focus on Office365 with passion for React and a hunger to learn more within the IT field</p>
-                    <p>This website will act like my CV including my portfolio</p>
-                </div>
-                <h3><Geolocation /><br/>Based in Stockholm</h3>
-                <a className="contact-me-btn" href="/contact">Contact me</a>
+        <div>
+            <div id="title-section" className="presentation-content">
+                <h1 id="name" className="title-content">KEVIN LANE</h1>
+                <h3 id="job-title" className="title-content">JavaScript & Office365 Developer</h3>
             </div>
+            <div id="description-section" className="presentation-content">
+                <p>Junior JavaScript Developer with focus on Office365 with passion for React and a hunger to learn more within the IT field</p>
+            </div>
+            <div id="location-section" className="presentation-content">
+                <Geolocation /><h2>BASED IN STOCKHOLM</h2>
+            </div>
+            <a className="contact-me-btn" href="/contact">Contact me</a>
+
+
         </div>
     );
 }
