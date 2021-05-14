64771ae3a229d3d383dccd5078aa14a721b4157d
kevin-lane <kevinlane3@hotmail.com>
2021-05-15 01:40:17 +0200

    The Blog page has ability to navigate to each individual blog post. Using mock data now.

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import BlogPost from './Blog/BlogPost';
 
-export default function Blog(){
+//Mock data
+let posts = [
+    { id: 1, date: "2020-12-12", heading: "Lorem Ipsum Color blablabla", introduction:"consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis. Amet porttitor eget dolor morbi.", entry:"Dvfdvfdvfdvdvfv cde dignissim enim sit."},
+    { id: 2, date: "2020-10-13", heading: "Lorem Ipsum Color lalalalalal", introduction:"consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.", entry:"Dweretyetteytrque dignissim enim sit."},
+    { id: 3, date: "2021-04-12", heading: "Lorem Ipsum Color bobobobobbobo", introduction:"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim crolor morbi.", entry:"Dvdfvfdvdvdbbvgvdfgvfdgdrgrdfdvfrdvfdm enim sit."}
+];
+
+export default function Blog(props){
+    console.log(posts);
+    const [hash, setHash] = useState(window.location.hash);
+    //Handle events when URL hash changes and reload page without manually refresh page
+    const hashHandler = () => {
+        setHash((prevHash) => {
+            const newHash = window.location.hash;
+            if(prevHash !== newHash){
+                return newHash;
+            }
+            return prevHash;
+        });
+    };
+    window.addEventListener('hashchange', hashHandler);
+
     return(
         <div>
-            <h1>No blog posts yet</h1>
+            {posts.map(post => {    
+                switch (window.location.hash) {
+                    case `#${post.id}`:
+                        return( <BlogPost id={post.id} date={post.date} heading={post.heading} introduction={post.introduction} entry={post.entry} /> );                    
+                    case '':                             
+                        return(
+                            <ul className="blog-posts">
+                                <a className="blog-post-item" id={post.id} href={`#${post.id}`}>
+                                     <BlogPost id={post.id} date={post.date} heading={post.heading} entry={post.entry} />
+                                </a>
+                            </ul>
+                        );                       
+                }
+            })} 
         </div>
-    );
+    )
 }
---
+++ src/Components/Blog/BlogPost.jsx
@@ -0,0 +1,19 @@
+export default function BlogPost(props){
+    return(
+        <div id={props.id} className="blog-post">
+            <div className="blog-post-heading"><h1>{props.heading}</h1></div>
+            <div className="blog-post-stamp"><p>{props.date}</p></div>
+            <div className="post-image"></div>
+            <div className="blog-post-introduction">
+                <h3>
+                    {props.introduction}
+                </h3>
+            </div>
+            <div className="blog-post-entry">
+                <p>
+                    {props.entry}
+                </p>
+            </div>
+        </div>
+    );
+}
