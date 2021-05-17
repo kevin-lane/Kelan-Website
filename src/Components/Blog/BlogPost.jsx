8d9f4105bfcfa11a213fcda7e304a237e0ee4c4a
kevin-lane <kevinlane3@hotmail.com>
2021-05-17 15:27:48 +0200

    Blog site is dont but currently with some mock data

--- src/Components/Blog/BlogPost.jsx
+++ src/Components/Blog/BlogPost.jsx
@@ -2,8 +2,9 @@ export default function BlogPost(props){
     return(
         <div id={props.id} className="blog-post">
             <div className="blog-post-heading"><h1>{props.heading}</h1></div>
+            <div className="post-image"><img src={props.image} alt="Girl in a jacket" width="800" height="400" /></div>
             <div className="blog-post-stamp"><p>{props.date}</p></div>
-            <div className="post-image"></div>
+            
             <div className="blog-post-introduction">
                 <h3>
                     {props.introduction}
