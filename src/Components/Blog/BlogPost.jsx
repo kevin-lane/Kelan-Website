7ed9d7433b107c1ac160dbffa8e718944d008b30
kevin-lane <kevinlane3@hotmail.com>
2021-07-02 18:01:39 +0200

    Some clean up

--- src/Components/Blog/BlogPost.jsx
+++ src/Components/Blog/BlogPost.jsx
@@ -8,7 +8,7 @@ export default function BlogPost(props){
         <div id={props.id} className="blog-post">
             <span className="return-back-btn" onClick={props.goBack}>Back</span>
             <div className="blog-post-heading"><h1>{props.heading}</h1></div>
-            <div className="post-image"><img src={props.image} alt="Girl in a jacket" width="800" height="400" /></div>
+            <div className="post-image"><img src={props.image} alt="imaginem" width="800" height="400" /></div>
             <div className="blog-post-stamp"><p>{props.date}</p></div>
             
             <div className="blog-post-introduction">
